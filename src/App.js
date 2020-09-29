0f1c2038eca7925693b4b273ecbdcc4f95338aec
Simone Padilha <simonemariano1822@gmail.com>
2020-09-29 18:28:03 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,33 +1,80 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form'
 
 import data from './data'
 function App() {
   const [jogos, setJogos] = useState(data)
+  const [editar, setEditar] = useState(false)
+  const [jogoSelecionado, setJogoSelecionado] = useState()
+  const [totalRecorde, setTotalRecorde] = useState(0)
+  const [minTemporadas, setMinTemporadas] = useState(0)
+  const [maxTemporadas, setMaxTemporadas] = useState(0)
   const { handleSubmit, reset, register, setValue } = useForm()
 
   function submit(data) {
-    var jogo = { codigo: jogos.length + 1, ...data }
-    setJogos([...jogos, jogo])
+    if (jogoSelecionado) {
+      let jogosAlterados = jogos.flatMap(jogo => {
+        if (jogo.codigo === jogoSelecionado.codigo) {
+          jogo.maxTemporada = data.maxTemporada
+          jogo.minTemporada = data.minTemporada
+          jogo.placar = data.placar
+          jogo.quebraMaxTemporada = data.quebraMaxTemporada
+          jogo.quebraMinTemporada = data.quebraMinTemporada
+        }
+        return jogo
+      })
+      setJogoSelecionado(null)
+      setEditar(false)
+      setJogos(jogosAlterados)
+    } else {
+      var jogo = { codigo: jogos.length + 1, ...data }
+      setJogos([...jogos, jogo])
+
+    }
+
     reset()
   }
 
-  function editar(jogo) {
+  function handleEditar(jogo) {
+    setEditar(true)
+    setJogoSelecionado(jogo)
     setValue("placar", jogo.placar)
     setValue("minTemporada", jogo.minTemporada)
     setValue("maxTemporada", jogo.maxTemporada)
     setValue("quebraMinTemporada", jogo.quebraMinTemporada)
     setValue("quebraMaxTemporada", jogo.quebraMaxTemporada)
   }
+
+  function calculoRecorde(jogos) {
+    var vezes = 0
+    jogos.map(jogo => {
+      vezes += parseInt(jogo.quebraMinTemporada)
+      vezes += parseInt(jogo.quebraMaxTemporada)
+    })
+    setTotalRecorde(vezes)
+  }
+
+  function calculoMinMaxTemporadas(jogos) {
+    var valores = []
+    jogos.map(jogo => {
+      valores.push(jogo.placar)
+    })
+    setMinTemporadas(Math.min(...valores))
+    setMaxTemporadas(Math.max(...valores))
+  }
+  useEffect(() => {
+    calculoRecorde(jogos)
+    calculoMinMaxTemporadas(jogos)
+  }, [jogos])
   return (
     <div className="App">
       <form onSubmit={handleSubmit(submit)}>
-        <input name="placar" placeholder="placar" ref={register} />
-        <input name="minTemporada" placeholder="minTemporada" ref={register} />
-        <input name="maxTemporada" placeholder="maxTemporada" ref={register} />
-        <input name="quebraMinTemporada" placeholder="quebraMinTemporada" ref={register} />
-        <input name="quebraMaxTemporada" placeholder="quebraMaxTemporada" ref={register} />
-        <button>salvar</button>
+        <input type="number" required name="placar" placeholder="placar" ref={register} />
+        <input type="number" required name="minTemporada" placeholder="minTemporada" ref={register} />
+        <input type="number" required name="maxTemporada" placeholder="maxTemporada" ref={register} />
+        <input type="number" required name="quebraMinTemporada" placeholder="quebraMinTemporada" ref={register} />
+        <input type="number" required name="quebraMaxTemporada" placeholder="quebraMaxTemporada" ref={register} />
+        <button>{editar ? 'alterar' : 'salvar'}</button>
       </form>
       <table border="1">
         <thead>
@@ -65,13 +112,18 @@ function App() {
               <td> {jogo.quebraMinTemporada}</td>
               <td> {jogo.quebraMaxTemporada}</td>
               <td>
-                <button type="button" onClick={() => editar(jogo)}>editar</button>
+                <button type="button" onClick={() => handleEditar(jogo)}>editar</button>
               </td>
             </tr>
           ))}
 
         </tbody>
       </table>
+      <ul>
+        <li>total quebra de recorde: {totalRecorde}</li>
+        <li>total minimo temporada: {minTemporadas}</li>
+        <li>total maximo temporada: {maxTemporadas}</li>
+      </ul>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,39 +1,39 @@
-const data= [
+const data = [
     {
-        codigo:1,
-        placar:12,
-        minTemporada:12,
-        maxTemporada:12,
-        quebraMinTemporada:12,
-        quebraMaxTemporada:12,
+        codigo: 1,
+        placar: 12,
+        minTemporada: 12,
+        maxTemporada: 12,
+        quebraMinTemporada: 0,
+        quebraMaxTemporada: 0,
 
     },
     {
-        codigo:2,
-        placar:12,
-        minTemporada:12,
-        maxTemporada:12,
-        quebraMinTemporada:12,
-        quebraMaxTemporada:12,
+        codigo: 2,
+        placar: 24,
+        minTemporada: 12,
+        maxTemporada: 24,
+        quebraMinTemporada: 0,
+        quebraMaxTemporada: 1,
 
     },
     {
-        codigo:3,
-        placar:12,
-        minTemporada:12,
-        maxTemporada:12,
-        quebraMinTemporada:12,
-        quebraMaxTemporada:12,
+        codigo: 3,
+        placar: 10,
+        minTemporada: 10,
+        maxTemporada: 24,
+        quebraMinTemporada: 1,
+        quebraMaxTemporada: 1,
 
     },
     {
-        codigo:4,
-        placar:12,
-        minTemporada:12,
-        maxTemporada:12,
-        quebraMinTemporada:12,
-        quebraMaxTemporada:12,
+        codigo: 4,
+        placar: 24,
+        minTemporada: 10,
+        maxTemporada: 24,
+        quebraMinTemporada: 1,
+        quebraMaxTemporada: 1,
 
     }
 ]
-module.exports=data
+module.exports = data
